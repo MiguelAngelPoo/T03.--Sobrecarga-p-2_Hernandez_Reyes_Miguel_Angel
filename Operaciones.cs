3684f37b65d8c72c44adf3100c2a322554f96994
MiguelAngelPoo <55799100+MiguelAngelPoo@users.noreply.github.com>
2019-09-25 07:50:48 -0700

    Add files via upload

---
+++ Operaciones.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SobrecargA
+{
+    class Operaciones
+    {
+
+
+      //creacion de metodos para sobrecarga
+
+        
+        //metodos donde se realiza las operacion 
+        public  double Multiplicacion(double a, double b, double c) 
+        {
+            double res;
+
+            res = a * b * c;
+           //retorna el valor res
+            return res;
+ 
+        }
+       
+        public  double Multiplicacion(double a, double b)
+        {
+            double res;
+            res = a * b;
+            return res;
+
+        }
+
+        public  double Multiplicacion(double a)
+        {
+            double res;
+            res = a * 5;
+            return res;
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SobrecargA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //mandar a llamar a los metodos y clases y mostrar sus resultados
+            Operaciones Op = new Operaciones();
+            //instanciar
+            var res1 = Op.Multiplicacion(2, 3, 4);
+            var res2 = Op.Multiplicacion(2,4);
+            var res3 = Op.Multiplicacion(2);
+            Console.ReadKey();
+            Datos d = new Datos();
+            d.obtenerDatos();
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
